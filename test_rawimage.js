84aadcbb0997baa8a645e34dca01a49dfbda2ecf
Nathan Guilhot <44872264+NathanGuilhot@users.noreply.github.com>
2026-04-18 18:31:38 +0200

    .gitignore

--- test_rawimage.js
+++
@@ -1,4 +0,0 @@
-import { RawImage } from '@xenova/transformers';
-console.log('RawImage static methods:', Object.keys(RawImage).filter(k => typeof RawImage[k] === 'function'));
-console.log('RawImage.from?', 'from' in RawImage);
-console.log('RawImage.fromBuffer?', 'fromBuffer' in RawImage);
--- test_rawimage2.js
+++
@@ -1,7 +0,0 @@
-import * as transformers from '@xenova/transformers';
-console.log('RawImage' in transformers ? transformers.RawImage : 'none');
-console.log('Object.keys(transformers):', Object.keys(transformers));
-// Check if RawImage is a property of pipeline maybe?
-// Let's see if we can create a pipeline and inspect
-const pipeline = await transformers.pipeline('feature-extraction', 'Xenova/clip-vit-base-patch32');
-console.log('pipeline created');
