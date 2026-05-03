73bee8c4a4e5376d317f5788359842220fce5c03
Nathan Guilhot <44872264+NathanGuilhot@users.noreply.github.com>
2026-05-03 11:28:55 +0200

    renderer: clear filters and queries on app load

--- electron/src/renderer/App.tsx
+++ electron/src/renderer/App.tsx
@@ -6,11 +6,24 @@ import { OnboardingTakeover } from './components/OnboardingTakeover';
 import { useErrorToastBridge } from './stores/useErrorToastBridge';
 import { useFolderStore } from './stores/folderStore';
 import { useOnboardingStore } from './stores/onboardingStore';
+import { useImageStore } from './stores/imageStore';
+import { usePinterestImportStore } from './stores/pinterestImportStore';
+import { useUIStore } from './stores/uiStore';
 
 function App() {
   useErrorToastBridge();
   const loadFolders = useFolderStore((s) => s.load);
   const loadOnboarding = useOnboardingStore((s) => s.load);
+  const clearFilters = useUIStore((s) => s.clearFilters);
+  const clearImageQuery = useImageStore((s) => s.clearImageQuery);
+  const resetPinterestImport = usePinterestImportStore((s) => s.reset);
+
+  useEffect(() => {
+    clearFilters();
+    clearImageQuery();
+    resetPinterestImport();
+  }, [clearFilters, clearImageQuery, resetPinterestImport]);
+
   useEffect(() => {
     void loadFolders();
     void loadOnboarding();
